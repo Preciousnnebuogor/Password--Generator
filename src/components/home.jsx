35828eaa2086f3900fbac130978e64e59ebb122b
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2025-07-21 22:50:33 -0700

    adding functionality

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,5 +1,27 @@
+import { useState } from "react";
 import { FaRegCopy } from "react-icons/fa6";
 export default function Home() {
+  const [getValue, setGetValue] = useState(12);
+  const [uppercase, setUpperCase] = useState(false);
+  const [lowercase, setLowerCase] = useState(false);
+  const [numbers, setNumbers] = useState(false);
+  const [symbol, setSymbol] = useState(false);
+
+  let characters = "";
+  if (uppercase) characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (lowercase) characters += "abcdefghijkmlmnopqrstuvwxyz";
+  if (numbers) characters += "0123456789";
+  if (symbol) characters += "!@#$%^&*()_+-=[]{};:'\"<>,.?/|\\~`";
+
+  function handleSubmit(){
+    if(!uppercase && !lowercase && !numbers && !symbol) {
+      alert("click on any of the checkbox") 
+      return
+    }
+  }
+
+
+  
   return (
     <div className="container">
       <div className="content">
@@ -16,27 +38,46 @@ export default function Home() {
           <div className="password-range">
             <label className="label1">Password Length</label>
             <div className="range">
-              <input type="range" min={"6"} max={"20"} value={"12"} readOnly />
-              <span>12</span>
+              <input
+                type="range"
+                min={"6"}
+                max={"20"}
+                value={getValue}
+                onChange={(e) => setGetValue(Number(e.target.value))}
+                readOnly
+              />
+              <span>{getValue}</span>
             </div>
           </div>
 
           <div>
             <div className="checkbox">
               <label>Include UpperCase</label>
-              <input type="checkbox" />
+              <input
+                type="checkbox"
+                onChange={(e) => setUpperCase(e.target.checked)}
+              />
             </div>
             <div className="checkbox">
               <label>Include LowerCase</label>
-              <input type="checkbox" />
+              <input
+                type="checkbox"
+                onChange={(e) => setLowerCase(e.target.checked)}
+              />
             </div>
             <div className="checkbox">
               <label>Include Numbers</label>
-              <input type="checkbox" />
+              <input
+                type="checkbox"
+                onChange={(e) => setNumbers(e.target.checked)}
+              />
             </div>
             <div className="checkbox">
               <label>Include Symbols</label>
-              <input type="checkbox" />
+              <input
+                type="checkbox"
+                onChange={(e) => setSymbol(e.target.checked)}
+              />
             </div>
           </div>
 
